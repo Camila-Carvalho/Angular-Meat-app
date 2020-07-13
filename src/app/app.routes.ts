f1542d734d013c65e19dd101eb0054704683647d
“Camila <camilacarvalho@sou.faccat.br>
2020-07-13 09:53:03 -0300

    Adicionado função de carrinho de compra, onde os itens são adicionados e excluidos. Também foi criada o componente de compra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,9 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { OrderComponent } from './order/order.component';
 import { AboutComponent } from './about/about.component';
+
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent}, //quando não especificar caminho, pega o componente principal
@@ -17,5 +19,6 @@ export const ROUTES: Routes = [
             {path: 'reviews', component: ReviewsComponent} //aqui é para navegar em avaliações
         ]
     },
+    {path: 'order', component: OrderComponent},
     {path: 'about', component: AboutComponent}
 ]
---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -0,0 +1,36 @@
+import { MenuItem } from './../menu-item/menu-item.model';
+import { CartItem } from './cart-Item.model';
+
+export class ShoppingCartService{
+
+    items: CartItem[] = []
+
+    clear(){
+        this.items = []
+    }
+
+    //adiciona um item do menu no carrinho
+    addItem(item:MenuItem){
+        //O método find() retorna se o parametro passado pra ele (mItem) é igual ao item que esta comparando
+        let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
+        if(foundItem){
+            foundItem.quantity = foundItem.quantity + 1 //se encontrar o item, acrescenta 1
+        }
+        else{
+            this.items.push(new CartItem(item)) //se não, inclui no carrinho
+        }
+
+    }
+    //remove um item do carrinho no carrinho
+    removeItem(item:CartItem){
+        this.items.splice(this.items.indexOf(item),1)
+    }
+
+    total(): number {
+        return this.items.map(item => item.value()).reduce((prev, value) => prev + value, 0)
+        //1º faz o map para substituir o cartItem pelo valor do cartItem (valor qjá é a quantidade + preço, porque value é a função do Cart-Item)
+        //2º a função reduce vai acumulando o valor total, ela pega o valor anterior (prev - acumulador) e soma com o valor do item (value)
+        //3º o zero é para caso não tenha nenhum valor, ele deixar com valor 0 no total
+    }
+
+}
