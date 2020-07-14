f4e2fafa93cb4f3d0f8fe63d42b981bbb0fa1ee6
“Camila <camilacarvalho@sou.faccat.br>
2020-07-14 17:48:53 -0300

    Acrescentado o componente da página após a finalização da venda

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,7 +1,9 @@
+import { MenuItem } from './../restaurant-detail/menu-item/menu-item.model';
 import { Component, OnInit } from '@angular/core';
 import { RadioOption } from './../shared/radio/radio-option.model';
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-Item.model';
 import { OrderService } from './order.service';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -44,7 +46,14 @@ export class OrderComponent implements OnInit {
     this.orderService.remove(item)
   }
 
-  checkOrder(order: any){
+  checkOrder(order: Order){
+  //para adicionar os itens do carrinho, é utilizado o map para "transformar" os itens de CartItem em OrderItem (que tem dois parametros)
+    order.orderItems = this.cartItems()
+      .map((item:CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order)//método para enviar os dados para a função que envia para API
+    .subscribe((orderId:string)=>{console.log(`Compra concluída: ${orderId}`)
+    this.orderService.clear()
+    })
     console.log(order)
   }
 }
--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from "@angular/core";
 
-import { CartItem } from './../restaurant-detail/shopping-cart/cart-Item.model';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map'
+import { MEAT_API } from './../app.api';
 
+import { Order, OrderItem } from "./order.model";
+import { CartItem } from './../restaurant-detail/shopping-cart/cart-Item.model';
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
 
 
@@ -9,7 +14,7 @@ import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shoppi
 @Injectable() //1---> pois será necessário injetar o serviço de shopping cart
 export class OrderService {
 
-    constructor(private cartService: ShoppingCartService) { //2---> Adicionar o serviço de shoppingCartService
+    constructor(private cartService: ShoppingCartService, private http: Http) { //2---> Adicionar o serviço de shoppingCartService
     }
 
     cartItems(): CartItem[] {//3---> Função para mostrar os itens
@@ -30,6 +35,23 @@ export class OrderService {
 
     itemsValue(item: CartItem) {
         return this.cartService.total()
+    }
+
+    clear(){
+        this.cartService.clear()
+    }
+
+    //método para enviar para API.JSON
+    checkOrder(order: Order): Observable<string>{
+        const headers = new Headers() //necessário informar o tipo do conteúdo que está passando, por isso é necessário o header
+        headers.append('Content-Type', 'aplication/json') //no header é necessário passar o nome do header e o valor dele
+        return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), new RequestOptions({headers: headers})).map(response=>response.json()).map(order => order.id)
+        /*
+        return this.http.post ---> retorna a compra
+        (`${MEAT_API}/orders`, ---> coloca na pasta orders
+        JSON.stringify(order)) ---> objeto que vai ser mandado, está em stringify porque em http a representação é textual 
+        new RequestOptions({headers: headers}) ---> aqui é pra passar os headers
+        */
     }
     //Depois de pronto necessário expor no componente
 }
