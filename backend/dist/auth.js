9b53981c9d9c51ba97a771b00eae2747d2f16ccf
“Camila <camilacarvalho@sou.faccat.br>
2020-07-20 09:37:00 -0300

    Acrescentado backend de login e autenticação

---
+++ backend/dist/auth.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+exports.handleAuthentication = void 0;
+var users_1 = require("./users");
+exports.handleAuthentication = function (req, resp) {
+    //código que vai processar o post do login
+    var user = req.body;
+    if (isValid(user)) {
+        var dbUser = users_1.users[user.email];
+        resp.json({ name: dbUser.name, email: dbUser.email });
+    }
+    else {
+        resp.status(403).json({ message: 'Dados inválidos.' });
+    }
+};
+function isValid(user) {
+    if (!user) {
+        return false;
+    }
+    var dbUser = users_1.users[user.email];
+    return dbUser !== undefined && dbUser.metches(user);
+}
